34ce5991c1819e1bb174d07bc14f79c7c0b4bf79
aaron <127000351+aaron-azarcon@users.noreply.github.com>
2024-09-19 15:02:11 +0800

    Add pubspec.lock
    Resolve dependency conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:cc206_benta/features/signup_page.dart';
+
 
 void main() {
   runApp(MaterialApp(
         title: 'Benta',
           theme: ThemeData.light(),
-          home: HomeView()
+  
       )
   );
 }
