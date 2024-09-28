a351863dbf4c48c3a726301336e56fc75c2edaed
keanedalisay <keandals21@gmail.com>
2024-09-28 15:46:23 +0800

    Add scrolling to home view

--- lib/features/home_view.dart
+++ lib/features/home_view.dart
@@ -7,41 +7,43 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body:
-        Column(
-          children: [
-            Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.all(20),
-                color: Color.fromRGBO(25, 170, 255, .8),
-                child: Text('Benta your way to Business',
-                    style: CustomTextStyle.H1)
-            ),
-            Container(
-                child: Image.asset('assets/images/business_as.jpg',
-                    fit: BoxFit.cover)
-            ),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-              child: Column(
+            ListView(children: [
+              Column(
                 children: [
-                  Text("What services do we offer?"),
-                  Text("This is my point 1."),
-                  Text("This is my point 2."),
-                  Text("This is my point 3."),
-                  Text("This is my point 4.")
+                  Container(
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(20),
+                      color: Color.fromRGBO(25, 170, 255, .8),
+                      child: Text('Benta your way to Business',
+                          style: CustomTextStyle.H1)
+                  ),
+                  Container(
+                      child: Image.asset('assets/images/business_as.jpg',
+                          fit: BoxFit.cover)
+                  ),
+                  Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      child: Column(
+                        children: [
+                          Text("What services do we offer?"),
+                          Text("This is my point 1."),
+                          Text("This is my point 2."),
+                          Text("This is my point 3."),
+                          Text("This is my point 4.")
+                        ],
+                      )
+                  ),
+                  Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      child: Text('Want to contact us? Call at 0999-999-9999!',
+                          style: CustomTextStyle.Small
+                      )
+                  )
                 ],
               )
-            ),
-            Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                child: Text('Want to contact us? Call at 0999-999-9999!',
-                style: CustomTextStyle.Small
-                )
-            )
-          ],
-        )
+            ])
     );
   }
 }
