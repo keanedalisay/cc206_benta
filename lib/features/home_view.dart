1ed7f8b9bfc01a826ef4d61170a814922b1f25b7
keanedalisay <keandals21@gmail.com>
2024-10-25 19:18:51 +0800

    Refactor home view content and styling

--- lib/features/home_view.dart
+++ lib/features/home_view.dart
@@ -6,68 +6,67 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:
-            ListView(children: [
-              Column(
-                children: [
-                  Container(
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.all(20),
-                      color: Color.fromRGBO(25, 170, 255, .8),
-                      child: Text('Benta your way to Business',
-                          style: CustomTextStyle.H1)
-                  ),
-                  Container(
-                      child: Image.asset('assets/images/business_as.jpg',
-                          fit: BoxFit.cover)
-                  ),
-                  Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                      child: Column(
-                        children: [
-                          Text("What services do we offer?", style: CustomTextStyle.H2),
-                          Text("This is my point 1.", style: CustomTextStyle.Body),
-                          Text("This is my point 2.", style: CustomTextStyle.Body),
-                          Text("This is my point 3.", style: CustomTextStyle.Body),
-                          Text("This is my point 4.", style: CustomTextStyle.Body)
-                        ],
-                      )
-                  ),
-                  Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                      child: Text('Want to contact us? Call at 0999-999-9999!',
-                          style: CustomTextStyle.Small
-                      )
-                  )
-                ],
-              )
-            ])
-    );
+        body: ListView(children: [
+      Container(
+          height: MediaQuery.sizeOf(context).height,
+          color: Color.fromRGBO(87, 144, 8, 1),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  child: Image.asset(
+                      'images/benta_logo_full_tagline_1280_x_720.png')),
+              Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                  child: Column(
+                    children: [
+                      Container(
+                          margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                          child: Text('New to Benta?',
+                              style: CustomTextStyle.TextBoldWhite)),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text('Sign-up',
+                              style: CustomTextStyle.TextBoldBlack),
+                          style: TextButton.styleFrom(
+                              fixedSize: Size(215, 45),
+                              backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+                              foregroundColor: Color.fromRGBO(31, 31, 31, 1)))
+                    ],
+                  )),
+              Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 50),
+                  child: Column(
+                    children: [
+                      Container(
+                          margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                          child: Text('Have an account?',
+                              style: CustomTextStyle.TextBoldWhite)),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text('Log-in',
+                              style: CustomTextStyle.TextBoldBlack),
+                          style: TextButton.styleFrom(
+                              fixedSize: Size(215, 45),
+                              backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+                              foregroundColor: Color.fromRGBO(31, 31, 31, 1)))
+                    ],
+                  ))
+            ],
+          ))
+    ]));
   }
 }
 
 class CustomTextStyle {
-  static const H1 = TextStyle(
-      fontSize: 30,
-      fontFamily: 'Arial',
-      fontWeight: FontWeight.w600
-  );
-
-  static const H2 = TextStyle(
-      fontSize: 24,
-      fontFamily: 'Arial',
-      fontWeight: FontWeight.w600
-  );
-
-  static const Body = TextStyle(
-      fontSize: 18,
-      fontFamily: 'Arial'
-  );
-
-  static const Small = TextStyle(
-      fontSize: 14,
-      fontFamily: 'Arial'
-  );
+  static const TextBoldWhite = TextStyle(
+      color: Color.fromRGBO(255, 255, 255, 1),
+      fontSize: 20,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w700);
+  static const TextBoldBlack = TextStyle(
+      color: Color.fromRGBO(31, 31, 31, 1),
+      fontSize: 20,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w700);
 }
