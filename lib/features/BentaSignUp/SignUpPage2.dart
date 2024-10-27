05e988bbe31b5f34b69377c55d48566d5665dbed
aaron <127000351+aaron-azarcon@users.noreply.github.com>
2024-10-27 15:01:13 +0800

    Fixed: Resolved Bottom Overflow Issue with Keyboard on Form

--- lib/features/BentaSignUp/SignUpPage2.dart
+++ lib/features/BentaSignUp/SignUpPage2.dart
@@ -9,141 +9,145 @@ class SignUpStep2 extends StatelessWidget {
       backgroundColor: const Color(0xFF579008),
       body: Padding(
         padding: const EdgeInsets.all(45.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+        child:
+        SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
 
-            Center(
-              child: Container(
-                width: 100,
-                height: 100,
-                child: Image.asset(
-                  'assets/images/benta_logo.png',
-                  fit: BoxFit.cover,
+              Center(
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset(
+                    'assets/images/benta_logo.png',
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            const Text(
-              'Sign-up',
-              style: TextStyle(
-                fontSize: 44,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-
-            const Text(
-              'Step 2',
-              style: TextStyle(
-                  fontSize: 28,
+              const Text(
+                'Sign-up',
+                style: TextStyle(
+                  fontSize: 44,
                   fontWeight: FontWeight.bold,
-                  color: Colors.white
-              ),
-            ),
-
-            const SizedBox(height: 40),
-
-            const Text(
-              'What will be your password?',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-
-            const SizedBox(height: 12),
-
-            // TextFormField for Email
-            TextFormField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25.0),
+                  color: Colors.white,
                 ),
               ),
-            ),
 
-            const SizedBox(height: 40),
-
-            const Text(
-              'Confirm Your User Password',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-
-            const SizedBox(height: 12),
-            // TextFormField for Password
-            TextFormField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25.0),
+              const Text(
+                'Step 2',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
                 ),
               ),
-            ),
 
-            const SizedBox(height: 60),
+              const SizedBox(height: 40),
 
-            // Sign Up Button
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SignUpPage()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(120, 55),
-                      backgroundColor: Colors.grey[200],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25),
-                        side: BorderSide.none,
-                      ),
-                    ),
-                    child: const Text(
-                      'Back',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
-                  ),
-
-                  const SizedBox(height: 40),
-
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SignUpPage3()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(120, 55),
-                      backgroundColor: Colors.grey[200],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25),
-                        side: BorderSide.none,
-                      ),
-                    ),
-                    child: const Text(
-                      'Finish',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
-                  ),
-
-                ],
+              const Text(
+                'What will be your password?',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
               ),
-            ),
-          ],
+
+              const SizedBox(height: 12),
+
+              // TextFormField for Email
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 40),
+
+              const Text(
+                'Confirm Your User Password',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+
+              const SizedBox(height: 12),
+              // TextFormField for Password
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 60),
+
+              // Sign Up Button
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignUpPage()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(120, 55),
+                        backgroundColor: Colors.grey[200],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          side: BorderSide.none,
+                        ),
+                      ),
+                      child: const Text(
+                        'Back',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+                      ),
+                    ),
+
+                    const SizedBox(height: 40),
+
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignUpPage3()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(120, 55),
+                        backgroundColor: Colors.grey[200],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          side: BorderSide.none,
+                        ),
+                      ),
+                      child: const Text(
+                        'Finish',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
