3f7fe819b6ba094ebed48f8968cd4596fb86e710
keanedalisay <keandals21@gmail.com>
2024-10-30 14:11:42 +0800

    Refactor theme color, theme fonts, and debug config

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:cc206_benta/features/home_view.dart';
 
 void main() {
-  runApp(MaterialApp(
-        title: 'Benta',
-          theme: ThemeData.light(),
+  runApp(
+      MaterialApp(
+          title: 'Benta',
+          color: Color.fromRGBO(87, 144, 8, 1),
+          themeMode: ThemeMode.light,
+          theme: ThemeData(
+            primaryColor: Color.fromRGBO(87, 144, 8, 1),
+            fontFamily: 'Inter',
+              fontFamilyFallback: ['Arial']
+          ),
+          debugShowCheckedModeBanner: true,
+          debugShowMaterialGrid: false,
           home: HomeView()
       )
   );
