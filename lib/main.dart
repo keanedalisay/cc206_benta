dcd3fd230a8196518acf28cee9f47deea9ef66f7
Marc Joshua Escueta <141274822+Ash631@users.noreply.github.com>
2024-10-30 10:22:15 +0800

    run^4
    amuja ang changes pinang ubra ko para mag work sya
    
    i used flutter create. --platforms=android

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,32 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'Inventory.dart';
 
 void main() {
   runApp(const InventoryApp()); // Run the world gurls
+=======
+
+import 'package:cc206_benta/features/inventory/inventory.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => InventoryModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp( // Remove 'const' here
+      home: InventoryApp(), 
+      
+    );
+  }
+>>>>>>> Stashed changes
 }
