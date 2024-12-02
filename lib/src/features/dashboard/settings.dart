65eb0e0bf3e69a8af5349264860ef94b3bec6b07
aaron <127000351+aaron-azarcon@users.noreply.github.com>
2024-12-03 00:32:00 +0800

    Add Settings Icon

--- lib/src/features/dashboard/settings.dart
+++ lib/src/features/dashboard/settings.dart
@@ -8,6 +8,38 @@ class Setting extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(36.0),
+        child: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(
+                  Icons.settings,
+                  size: 40,
+                  color: Colors.grey[600],
+                ),
+
+                const SizedBox(width: 8),
+
+                Text(
+                  'Settings',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'Inter',
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ],
+            ),
+
+          ],
+        ),
+
+      ),
       bottomNavigationBar: GeneralBottomNavBar(
           navItems: 2,
           navLabels: ['Back', 'Save'],
