66ee8264ebb9fd0e7842525e7430cc85ec5b7d0c
aaron <127000351+aaron-azarcon@users.noreply.github.com>
2024-12-03 00:14:26 +0800

    Add initial settings setup

---
+++ lib/src/features/dashboard/settings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cc206_benta/src/shared-components/custom-widgets/general-bottom-nav-bar.dart';
+
+class Setting extends StatelessWidget {
+  const Setting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
