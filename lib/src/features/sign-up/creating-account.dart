7c1c1cce22b69d6d796b7f8602ca02b321240d3d
keanedalisay <keandals21@gmail.com>
2024-12-02 16:31:04 +0800

    Refactor layout and colors of sign-up views

--- lib/src/features/sign-up/creating-account.dart
+++ lib/src/features/sign-up/creating-account.dart
@@ -15,45 +15,45 @@ class CreatingAccount extends StatelessWidget{
 
     return Scaffold(
       backgroundColor: const Color(0xFF579008),
-      body: Padding(
-        padding: const EdgeInsets.all(45.0),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Container(
+            height: MediaQuery.sizeOf(context).height,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(45.0),
+          child:
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-
-              const SizedBox(height: 80),
-
               Container(
-                width: 300,
-                height: 300,
+                width: 200,
+                height: 200,
                 child: Image.asset(
                   'assets/images/benta_logo_single_letter_200_x_200.png',
                   fit: BoxFit.cover,
                 ),
               ),
-
+              const SizedBox(height: 20),
               const Text(
                   "Creating",
                   style: TextStyle(
-                      fontSize: 50,
+                      fontSize: 40,
                       fontWeight: FontWeight.bold,
                       color: Colors.white
                   )
               ),
-
               const SizedBox(height: 20),
-
               const Text(
                   "your business and user account...",
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                      fontSize: 25,
+                      fontSize: 24,
                       color: Colors.white
                   )
               ),
-
             ]
-        ),
+          )
+        )
       ),
     );
   }
--- lib/src/features/sign-up/sign-up-1.dart
+++ lib/src/features/sign-up/sign-up-1.dart
@@ -1,4 +1,5 @@
 import 'package:cc206_benta/src/features/sign-up/auth_service.dart';
+import 'package:cc206_benta/src/shared-components/custom-widgets/general-text-button.dart';
 import 'package:flutter/material.dart';
 import 'package:cc206_benta/src/features/sign-up/auth_service.dart';
 import 'package:email_validator/email_validator.dart';
@@ -29,133 +30,156 @@ class _SignUp1State extends State<SignUp1> {
 
     return Scaffold(
       backgroundColor: Color(0xFF579008),
-      body: Padding(
-        padding: const EdgeInsets.all(45.0),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child:
         SingleChildScrollView(
           physics: const BouncingScrollPhysics(),
           child: Form(
             key: _formKey,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                const SizedBox(height: 40),
                 Center(
                   child: Container(
-                    width: 100,
-                    height: 100,
+                    width: 80,
+                    height: 80,
                     child: Image.asset(
                       'assets/images/benta_logo_single_letter_80_x_80.png',
                       fit: BoxFit.cover,
                     ),
                   ),
                 ),
-
+                const SizedBox(height: 20),
+                Container(
+                  width: 350,
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Sign-up',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        'Step 1',
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                    ]
+                  )
+                ),
                 const SizedBox(height: 30),
-
-                const Text(
-                  'Sign-up',
-                  style: TextStyle(
-                      fontSize: 44,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                  ),
+                Container(
+                  width: 350,
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'What is the name of your \nbusiness?',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                      const SizedBox(height: 10),
+                      // TextFormField for Business Name
+                      SizedBox(
+                          width: 350,
+                          height: 54,
+                          child: TextFormField(
+                            controller: _businessName,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 20,
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter the name of your business';
+                              }
+                              return null;
+                            },
+                          )
+                      ),
+                      const SizedBox(height: 30),
+                      const Text(
+                        'What is your email?',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                      const SizedBox(height: 12),
+                      // TextFormField for Password
+                      SizedBox(
+                          width: 350,
+                          height: 54,
+                          child: TextFormField(
+                              controller: _email,
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 10,
+                                  horizontal: 20,
+                                ),
+                              ),
+                              validator: (value){
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your email';
+                                } else if (!EmailValidator.validate(value)) {
+                                  return 'Please enter a valid email address';
+                                }
+                                return null;
+                              }
+                          )
+                      ),
+                    ]
+                  )
                 ),
-
-                const Text(
-                  'Step 1',
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
-                ),
-
                 const SizedBox(height: 40),
-
-                const Text(
-                  'What is the name of your business?',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-
-                const SizedBox(height: 12),
-
-                // TextFormField for Business Name
-                TextFormField(
-                  controller: _businessName,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the name of your business';
-                    }
-                    return null;
-                  },
-                ),
-
-                const SizedBox(height: 40),
-
-                const Text(
-                  'What is your email?',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-
-                const SizedBox(height: 12),
-                // TextFormField for Password
-                TextFormField(
-                  controller: _email,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                    ),
-                  ),
-                  validator: (value){
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    } else if (!EmailValidator.validate(value)) {
-                      return 'Please enter a valid email address';
-                    }
-                    return null;
-                  }
-                ),
-
-                const SizedBox(height: 60),
-
                 // Sign Up Button
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()){
-                      Navigator.pushNamed(
-                        context,
-                        '/sign-up/password',
-                        arguments: {'email': _email.text.trim()},
-                      );
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(120, 55),
-                    backgroundColor: Colors.grey[200],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                      side: BorderSide.none,
-                    ),
-                  ),
-                  child: const Text(
-                    'Next',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ],
+              Container(
+                width: 350,
+                  child:
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GeneralTextButton(
+                          label: 'Cancel',
+                          color: Color(0xFFFFFFFF),
+                          backgroundColor: Color(0xFF579008),
+                          onPressed: () => Navigator.pop(context)
+                      ),
+                      GeneralTextButton(
+                        label: 'Next',
+                        color: Color(0xFF212121),
+                        backgroundColor: Color(0xFFFFFFFF),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()){
+                            Navigator.pushNamed(
+                              context,
+                              '/sign-up/password',
+                              arguments: {'email': _email.text.trim()},
+                            );
+                          }
+                        },
+                        padding: 30
+                        )
+                    ],
+                  )
               ),
+                const SizedBox(height: 40),
             ],
             ),
           ),
