b27e5ec96304ec6779dc16919555e88b7c4973c9
keanedalisay <keandals21@gmail.com>
2024-12-02 16:31:04 +0800

    Refactor layout and colors of sign-up views

--- lib/src/features/sign-up/sign-up-2.dart
+++ lib/src/features/sign-up/sign-up-2.dart
@@ -5,6 +5,8 @@ import 'package:cc206_benta/src/features/sign-up/auth_service.dart';
 import 'package:cc206_benta/src/features/sign-up/sign-up-1.dart';
 import 'package:cc206_benta/src/features/sign-up/creating-account.dart';
 
+import '../../shared-components/custom-widgets/general-text-button.dart';
+
 
 class SignUp2 extends StatefulWidget{
 
@@ -50,189 +52,188 @@ class _SignUpState extends State<SignUp2> {
 
     return Scaffold(
       backgroundColor: const Color(0xFF579008),
-      body: Padding(
-        padding: const EdgeInsets.all(45.0),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child:
         SingleChildScrollView(
           physics: const BouncingScrollPhysics(),
           child: Form(
             key: _formKey,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-            
+                const SizedBox(height: 40),
                 Center(
                   child: Container(
-                    width: 100,
-                    height: 100,
+                    width: 80,
+                    height: 80,
                     child: Image.asset(
                       'assets/images/benta_logo_single_letter_80_x_80.png',
                       fit: BoxFit.cover,
                     ),
                   ),
                 ),
-            
                 const SizedBox(height: 20),
-            
-                const Text(
-                  'Sign-up',
-                  style: TextStyle(
-                    fontSize: 44,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
+                Container(
+                  width: 350,
+                    alignment: Alignment.centerLeft,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Sign-up',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        'Step 2',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                        ),
+                      ),
+                    ]
+                  )
                 ),
-            
-                const Text(
-                  'Step 2',
-                  style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white
-                  ),
+                const SizedBox(height: 30),
+                Container(
+                  width: 350,
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'What will be your password?',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      // TextFormField for Password
+                      SizedBox(
+                          width: 350,
+                          height: 54,
+                          child: TextFormField(
+                              obscureText: !_isVisible,
+                              decoration: InputDecoration(
+                                suffixIcon: IconButton(
+                                  onPressed: (){
+                                    setState(() {
+                                      _isVisible = !_isVisible;
+                                    });
+                                  },
+                                  icon: _isVisible
+                                      ? const Icon(Icons.visibility)
+                                      : const Icon(Icons.visibility_off),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 10,
+                                  horizontal: 20,
+                                ),
+                              ),
+                              onChanged: (value){
+                                _password = value;
+                              },
+                              validator: (value) {
+                                if (value == null || value.isEmpty){
+                                  return 'Please enter a password';
+                                } else if (value.length < 6){
+                                  return 'Password must be at least 6 characters long';
+                                }
+                                return null;
+                              }
+                          )
+                      ),
+                      const SizedBox(height: 30),
+                      const Text(
+                        'Confirm your user password',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      // TextFormField for Password Confirmation
+                      SizedBox(
+                          width: 350,
+                          height: 54,
+                          child: TextFormField(
+                              controller: _passwordController,
+                              obscureText: !_isVisible2,
+                              decoration: InputDecoration(
+                                suffixIcon: IconButton(
+                                  onPressed: (){
+                                    setState(() {
+                                      _isVisible2 = !_isVisible2;
+                                    });
+                                  },
+                                  icon: _isVisible2 ? const Icon(Icons.visibility) : const Icon(Icons.visibility_off),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 10,
+                                  horizontal: 20,
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty){
+                                  return 'Please confirm your password';
+                                } else if (value !=  _password){
+                                  return 'Passwords do not match';
+                                }
+                                return null;
+                              }
+                          )
+                      ),
+                    ]
+                  )
                 ),
-            
                 const SizedBox(height: 40),
-            
-                const Text(
-                  'What will be your password?',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-            
-                const SizedBox(height: 12),
-            
-                // TextFormField for Password
-                TextFormField(
-                  obscureText: !_isVisible,
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                      onPressed: (){
-                        setState(() {
-                          _isVisible = !_isVisible;
-                        });
-                      },
-                      icon: _isVisible
-                          ? const Icon(Icons.visibility)
-                          : const Icon(Icons.visibility_off),
-                    ),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                    ),
-                  ),
-                  onChanged: (value){
-                    _password = value;
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty){
-                      return 'Please enter a password';
-                    } else if (value.length < 6){
-                      return 'Password must be at least 6 characters long';
-                    }
-                    return null;
-                  }
-                ),
-            
-                const SizedBox(height: 40),
-            
-                const Text(
-                  'Confirm Your User Password',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-            
-                const SizedBox(height: 12),
-                // TextFormField for Password Confirmation
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: !_isVisible2,
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                      onPressed: (){
-                        setState(() {
-                          _isVisible2 = !_isVisible2;
-                        });
-                      },
-                      icon: _isVisible2 ? const Icon(Icons.visibility) : const Icon(Icons.visibility_off),
-                    ),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty){
-                      return 'Please confirm your password';
-                    } else if (value !=  _password){
-                      return 'Passwords do not match';
-                    }
-                    return null;
-                  }
-                ),
-            
-                const SizedBox(height: 60),
-            
                 // Sign Up Button
-                Center(
+                Container(
+                  width: 350,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => SignUp1()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(120, 55),
-                          backgroundColor: Colors.grey[200],
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25),
-                            side: BorderSide.none,
-                          ),
-                        ),
-                        child: const Text(
-                          'Back',
-                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
-                        ),
+                      GeneralTextButton(
+                          label: 'Cancel',
+                          color: Color(0xFFFFFFFF),
+                          backgroundColor: Color(0xFF579008),
+                          onPressed: () => Navigator.pop(context)
                       ),
-            
-                      const SizedBox(height: 40),
-            
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()){
-                            _signup(email);
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => (CreatingAccount())),
-                            );
+                      GeneralTextButton(
+                          label: 'Sign-up',
+                          color: Color(0xFF212121),
+                          backgroundColor: Color(0xFFFFFFFF),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()){
+                              _signup(email);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => (CreatingAccount())),
+                              );
+                            }
                           }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(120, 55),
-                          backgroundColor: Colors.grey[200],
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25),
-                            side: BorderSide.none,
-                          ),
-                        ),
-                        child: const Text(
-                          'Finish',
-                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
-                        ),
-                      ),
+                      )
                     ],
                   ),
                 ),
+                const SizedBox(height: 40),
               ],
             ),
           ),
